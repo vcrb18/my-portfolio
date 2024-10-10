1689c28dff6d6acd2dcf0694c1d2c7687708ed5f
Vicente Cruz <vcrb@uc.cl>
2024-10-10 19:50:34 -0300

    pre deleting blog

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
-const path = require('path')
- 
+const path = require("path");
+
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
       },
       {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
+        protocol: "https",
+        hostname: "media.dev.to",
+        pathname: "**",
       },
     ],
   },
-}
+  output: "export",
+};
